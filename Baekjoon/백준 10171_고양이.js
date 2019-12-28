901b6de5099c1f2396bff1de13b3f93bd25d3e06
Sol ji Choi <bssj9307@gmail.com>
2019-12-28 22:37:00 +0900

    191228 | Baekjoon | 고양이

---
+++ Baekjoon/백준 10171_고양이.js
@@ -0,0 +1,13 @@
+/*
+고양이 귀여워
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+
+const cat1 = '\\    /\\\n'
+const cat2 = ' )  ( \')\n'
+const cat3 = '(  /  )\n'
+const cat4 = ' \\(__)|\n'
+console.log(cat1+cat2+cat3+cat4)
