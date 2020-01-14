788df85d2a723de932fc7e1ef05be4ff5b6a08a6
Sol ji Choi <bssj9307@gmail.com>
2020-01-15 06:53:42 +0900

    2573

--- Baekjoon/백준 2573_빙산.py
+++ Baekjoon/백준 2573_빙산.py
@@ -2,25 +2,27 @@ import sys
 sys.stdin = open('2573.txt', 'r')
 
 
-def ice():  # 빙산 분리되는지 체크
-    global height, dxdy
-    cnt = 0
-    visited = [[0]*M for _ in range(N)]
-    for s, t, r in height:
-        stack = [(s, t)]
-        while stack:
-            a, b = stack.pop()
-            if not visited[a][b]:
-                visited[a][b] = 1
-                for da, db in dxdy:
-                    na, nb = a + da, b + db
-                    if 0 <= na < N and 0 <= nb < M:
-                        if not visited[na][nb]:
-                            if data[na][nb] != 0:
-                                stack += [(na, nb)]
-                            visited[na][nb] = 1
-        cnt += 1
-    return cnt
+# def ice():  # 빙산 분리되는지 체크
+#     global height, dxdy
+#     cnt = 0
+#     visited = [[0]*M for _ in range(N)]
+#     for s, t, r in height:
+#         if not visited[s][t] and data[s][t] != 0:
+#             stack = [(s, t)]
+#             a, b = stack[-1]
+#             while stack:
+#                 visited[a][b] = 1
+#                 for da, db in dxdy:
+#                     na, nb = a + da, b + db
+#                     if 0 <= na < N and 0 <= nb < M:
+#                         if not visited[na][nb]:
+#                             if data[na][nb] != 0:
+#                                 stack += [(na, nb)]
+#                                 a, b = na, nb
+#                                 # visited[na][nb] = 1
+#                                 break
+#             cnt += 1
+#     return cnt
 
 
 N, M = map(int, input().split())
@@ -31,27 +33,39 @@ for i in range(N):
     for j in range(M):
         if data[i][j]:
             height += [(i, j, 0)]
-
 dxdy = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 time = 0
 peng = 0
-while peng <= 2:
-    time += 1
+while peng < 2:
+    visited = [[0] * M for _ in range(N)]
     for k in range(len(height)):
         x, y, h = height[k]
-        temp = 0
-        for dx, dy in dxdy:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < M:
-                if data[nx][ny] == 0:
-                    temp += 1
-        height[k] = x, y, h-temp
-    for x2, y2, h2 in height:
+        if h != 0 and not visited[x][y]:
+            queue = [(x, y)]
+            front, rear = -1, 0
+            while front != rear:
+                front += 1
+                x, y = queue[front]
+                temp = 0
+                for dx, dy in dxdy:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M:
+                        if data[nx][ny] == 0:
+                            temp += 1
+                            queue += [(nx, ny)]
+                            visited[nx][ny] = 1
+                height[k] = x, y, data[x][y]-temp
+            peng += 1
+    for k2 in range(len(height)):
+        x2, y2, h2 = height[k2]
         if h2 <= 0:
             data[x2][y2] = 0
         else:
             data[x2][y2] = h2
-    peng = ice()
+    time += 1
 
-print(time)
+if peng >= 2:
+    print(time)
+else:
+    print(0)
