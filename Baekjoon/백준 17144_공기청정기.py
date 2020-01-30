8c67ffadb4354809c30a499a5492eb3a4f816050
Sol ji Choi <bssj9307@gmail.com>
2020-01-31 01:22:48 +0900

    17144 푸는중

--- Baekjoon/백준 17144_공기청정기.py
+++ Baekjoon/백준 17144_공기청정기.py
@@ -1,7 +1,13 @@
 import sys
 sys.stdin = open('17144.txt', 'r')
 
+
 R, C, T = map(int, input().split())
 home = [list(map(int, input().split())) for _ in range(R)]
 
 print(*home, sep='\n')
+def iswall(x, y):
+	return True if 0 <= x < R and 0 <= y < R else False
+
+def isair(x, y):
+	return True if home[x][y] != -1 else False
