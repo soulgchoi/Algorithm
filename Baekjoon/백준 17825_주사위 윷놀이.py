b6d070b2ce7d3fc2a57d92da6bc14fa1c7a786e4
Sol ji Choi <bssj9307@gmail.com>
2020-02-25 19:43:41 +0900

    백준 17825 푸는중

--- Baekjoon/백준 17825_주사위 윷놀이.py
+++ Baekjoon/백준 17825_주사위 윷놀이.py
@@ -1,58 +1,78 @@
-# 테케 4개
-
 import sys
 sys.stdin = open('17825.txt', 'r')
 
 dice = list(map(int, input().split()))
-
-print(dice)
-
-# 말은 네 개이다.
-
-# 각 칸의 값이 점수로 추가된다.
-# 화살표 방향대로 이동한다.
-# 10, 20, 30 에서 파란 화살표
-# 10, 20, 30 을 제외한 짝수는 모두 한 칸이다.
-# 10 이면 13, 16, 19, 25, 30, 35, 40
-# 20 이면 22, 24, 25, 30, ..
-# 30 이면 28, 27, 26, 25
-# 25이면 무조건 30, 35, 40, 도착
-# 외에는 2, 4, ... , 40 (10, 20, 30 제외)
-
-game = list(range(41))
-print(game)
-tf = [25, 30, 35, 40]
-blue1 = [10, 13, 16, 19]
-blue2 = [20, 22, 24]
-blue3 = [30, 28, 27, 26]
-for g in range(41):
-    if g % 2 and g not in (tf + blue1 + blue2 + blue3):
-        game[g] = 0
-# 위의 윷놀이 판에서 0 보다 클 때만 주사위 숫자를 - 한다.
-
-# 말은 항상 점수와 위치를 저장한다.
-# depth 로 10번 채우면 return 한다.
-# 가장 큰 값을 찾는다.
-# [0, 0, 0, 0]
-# 말 네개를 1, 0 초기화하며 들어간다.
-# 작으면 안간다.
-
-mal = [0, 0, 0, 0]
+mal = [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]]
 ans = 0
 
+boardRed = list(range(0, 39, 2))
+boardBlueOne = [10, 13, 16, 19]
+boardBlueTwo = [20, 22, 24]
+boardBlueThree = [30, 28, 27, 26]
+boardExtra = [25, 30, 35, 40]
+board = [boardRed, boardBlueOne, boardBlueTwo, boardBlueThree, boardExtra]
+
+visited = [
+    [0]*len(boardRed),
+    [0]*len(boardBlueOne),
+    [0]*len(boardBlueTwo),
+    [0]*len(boardBlueThree),
+    [0]*len(boardExtra)
+]
+
+def isBlue(x, y):
+    if board[mal[x][0]][mal[x][1]] == 10:
+        mal[x] = [1, 0, y]
+    elif board[mal[x][0]][mal[x][1]] == 20:
+        mal[x] = [2, 0, y]
+    elif mal[x][0] == 0 and board[mal[x][0]][mal[x][1]] == 30:
+        mal[x] = [3, 0, y]
+
 def solve(k, val):
-    global ans, mal, game, dice
+    global ans, visited
     if k == 10:
-        if val > ans:
-            ans = val
-            return
+        ans = max(val, ans)
     else:
         for i in range(4):
-            dk = dice[k]
-            g = 0
-            while dk > 0:
-                if game[g] == 0:
-                    g += 1
+            if mal[i][2]:
+                isBlue(i, mal[i][2])
+                a, b, c = mal[i]
+                if b + dice[k] > len(board[a])-1:
+                    if a == 4:
+                        # visited[a][b] = 0
+                        # mal[i][2] = 0
+                        # solve(k+1, val)
+                        # visited[a][b] = 1
+                        # mal[i][2] = 1
+                        continue
+                    else:
+                        temp = dice[k] - (len(board[a])-b)
+                        if temp > len(board[4])-1:
+                            # visited[a][b] = 0
+                            # mal[i][2] = 0
+                            # solve(k + 1, val)
+                            # visited[a][b] = 1
+                            # mal[i][2] = 1
+                            continue
+                        else:
+                            if not visited[4][temp]:
+                                visited[a][b] = 0
+                                mal[i] = [4, temp, 1]
+                                visited[4][temp] = 1
+                                solve(k + 1, val + board[4][temp])
+                                visited[a][b] = 1
+                                visited[4][temp] = 0
+                                mal[i] = [a, b, c]
                 else:
-                    if game[g] == 10:
-                        
+                    if not visited[a][b+dice[k]]:
+                        visited[a][b] = 0
+                        mal[i] = [a, b + dice[k], 1]
+                        isBlue(i, mal[i][2])
+                        visited[mal[i][0]][mal[i][1]] = 1
+                        solve(k+1, val+board[mal[i][0]][mal[i][1]])
+                        visited[a][b] = 1
+                        visited[mal[i][0]][mal[i][1]] = 0
+                        mal[i] = [a, b, c]
+
+solve(0, 0)
+print(ans)
