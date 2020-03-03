6c18a71eb756f17c35fa6b845e7251f60cf0d40d
Sol ji Choi <bssj9307@gmail.com>
2020-03-03 21:31:51 +0900

    swea 숫자 만들기

--- SWEA/4008 [모의 SW 역량테스트] 숫자 만들기.py
+++ SWEA/4008 [모의 SW 역량테스트] 숫자 만들기.py
@@ -1,37 +1,70 @@
-from itertools import permutations
 import sys
 sys.stdin = open('4008.txt', 'r')
+
+#
+# def func(k, val, op1, op2, op3, op4):
+# 	global ans_min, ans_max, N
+# 	if k == N:
+# 		if val < ans_min:
+# 			ans_min = val
+# 		if val > ans_max:
+# 			ans_max = val
+# 	else:
+# 		if op1 > 0:
+# 			func(k + 1, val + numbers[k], op1 - 1, op2, op3, op4)
+# 		if op2 > 0:
+# 			func(k + 1, val - numbers[k], op1, op2 - 1, op3, op4)
+# 		if op3 > 0:
+# 			func(k + 1, val * numbers[k], op1, op2, op3 - 1, op4)
+# 		if op4 > 0:
+# 			func(k + 1, int(val / numbers[k]), op1, op2, op3, op4 - 1)
+#
+#
+# T = int(input())
+# for tc in range(1, T+1):
+# 	N = int(input())
+# 	op1, op2, op3, op4 = map(int, input().split())
+# 	numbers = list(map(int, input().split()))
+#
+# 	ans_min = 10000000000
+# 	ans_max = -10000000000
+#
+# 	func(1, numbers[0], op1, op2, op3, op4)
+# 	print('#%d %d' % (tc, ans_max - ans_min))
 
 T = int(input())
 for tc in range(1, T+1):
 	N = int(input())
-	operators_input = list(map(int, input().split()))
-	# + - * / 순서
-	# 연산자 우선순위는 무시하고 앞부터 계산한다.
+	operators = list(map(int, input().split()))
 	numbers = list(map(int, input().split()))
 
-	temp = ['*', '-', '*', '/']
-	operators = []
-	for idx, val in enumerate(temp):
-		operators += [val] * operators_input[idx]
-	print(operators)
+	ans_min = 10000000000
+	ans_max = -10000000000
 
-	M = len(operators)
-	v = [0]*M
-
-	ans_min = 99999
-	ans_max = 0
 	def func(k, val):
+		global ans_min, ans_max
 		if k == N:
 			if val < ans_min:
 				ans_min = val
-			elif val > ans_max:
+			if val > ans_max:
 				ans_max = val
 		else:
-			for i in range(M):
-				if v[i]: continue
-				t[k] = arr[i]
-				v[i] = 1
-				func(k+1, val)
-				v[i] = 0
+			for i in range(4):
+				if operators[i] == 0: continue
+				operators[i] -= 1
+				t_val = func2(val, numbers[k], i)
+				func(k+1, t_val)
+				operators[i] += 1
 
+	def func2(a, b, c):
+		if c == 0:
+			return a + b
+		elif c == 1:
+			return a - b
+		elif c == 2:
+			return a * b
+		elif c == 3:
+			return int(a/b)
+
+	func(1, numbers[0])
+	print('#%d %d' % (tc, ans_max - ans_min))
