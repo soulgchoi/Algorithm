978276b4432e894f691c74336dcbd66585b41f2b
Solji Choi <bssj9307@gmail.com>
2020-08-17 16:56:44 +0900

    200817 | 완주하지 못한 선수

---
+++ Programmers/완주하지 못한 선수.js
@@ -0,0 +1,16 @@
+// 해시 문제라고 해서 값을 저장하는 방식으로 풀었는데,
+// 별로 좋은 코드는 아닌 것 같다..
+function solution(participant, completion) {
+    const answer = {};
+    participant.forEach((par) => {
+        if (answer[par]) {
+            answer[par] += 1
+        } else {
+            answer[par] = 1
+        }
+    })
+    completion.forEach((com) => {
+        answer[com] -= 1
+    })
+    return Object.keys(answer).find(key => answer[key] === 1);
+}
