1ae6158b9753f32e9a60b1ea4154b7f315f446ba
Solji Choi <bssj9307@gmail.com>
2020-08-18 22:15:05 +0900

    200818 | 같은 숫자는 싫어

--- Programmers/같은 숫자는 싫어.js
+++ Programmers/같은 숫자는 싫어.js
@@ -5,3 +5,13 @@ function solution(arr) {
           , [])
     return arr;
 }
+
+// 통과
+// 다른 메서드를 쓰는 게 더 오래걸리는 모양이다.
+function solution2(arr) {
+    const answer = []
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i-1] !== arr[i]) answer.push(arr[i])
+    }
+    return answer;
+}
