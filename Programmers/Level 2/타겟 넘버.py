463a525dad7489e1f3c6eb069ff1a0004ea5b427
Solji Choi <bssj9307@gmail.com>
2020-09-30 17:29:09 +0900

    200930 | 타겟 넘버(Python)

--- Programmers/Level 2/타겟 넘버.py
+++ Programmers/Level 2/타겟 넘버.py
@@ -12,3 +12,21 @@ def solution(numbers, target):
 
 	bfs(0, 0)
 	return len(answer)
+
+#
+answer = 0
+
+def bfs(arr, val, depth, k, tg, cnt):
+    global answer
+    if depth == k:
+        if val == tg:
+            answer += 1
+            return cnt
+    else:
+        bfs(arr, val + arr[depth], depth + 1, k, tg, cnt)
+        bfs(arr, val - arr[depth], depth + 1, k, tg, cnt)
+
+def solution(numbers, target):
+    global answer
+    bfs(numbers, 0, 0, len(numbers), target, 0)
+    return answer
