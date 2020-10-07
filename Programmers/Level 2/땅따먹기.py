bc2bca04eb9374b89be5067de5c2585424d155f7
Solji Choi <bssj9307@gmail.com>
2020-10-07 11:43:40 +0900

    201007 | 땅따먹기(Python)

--- Programmers/Level 2/땅따먹기.py
+++ Programmers/Level 2/땅따먹기.py
@@ -1,3 +1,19 @@
+# dp로 풀기
+def solution(land):
+	l = len(land)
+	arr = [[0] * 4 for _ in range(l)]
+	for i in range(4):
+		arr[0][i] = land[0][i]
+
+	for a in range(1, l):
+		for b in range(4):
+			for c in range(4):
+				if b != c:
+					arr[a][b] = max(arr[a][b], land[a][b] + arr[a - 1][c])
+
+	return max(arr[-1])
+
+
 # 테스트 케이스는 맞는데 실행하면 런타임 에러
 # 재귀가 아니라 dp로 풀어야 하는 것 같다..
 def solution(land):
