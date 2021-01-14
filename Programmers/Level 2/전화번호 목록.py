8db9ed5eaff816c21b6c8e5414a2a76899944fef
Solji Choi <bssj9307@gmail.com>
2021-01-14 16:07:30 +0900

    210114 | 전화번호 목록(Python)

--- Programmers/Level 2/전화번호 목록.py
+++ Programmers/Level 2/전화번호 목록.py
@@ -8,3 +8,34 @@ def solution(phone_book):
                 return False
         i += 1
     return True
+
+
+def solution2(phone_book):
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = []
+        temp = ""
+        for number in phone_number:
+            temp += number
+            hash_map[phone_number] += [temp]
+
+    for phone_number in phone_book:
+        for key, val in hash_map.items():
+            if phone_number != key and phone_number in val:
+                return False
+    return True
+
+
+def solution3(phone_book):
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if phone_number != temp and temp in phone_book:
+                return False
+    return True
+
+
+# print(solution(["119", "97674223", "1195524421"]))
+# print(solution(	["123", "456", "789"]))
+# print(solution(	["12", "123", "1235", "567", "88"]))
