c8b11000c4fe288f23e9ea93da606f26432021af
Solji Choi <bssj9307@gmail.com>
2021-01-14 18:27:27 +0900

    210114 | 위장

---
+++ Programmers/Level 2/위장.py
@@ -0,0 +1,18 @@
+def solution(clothes):
+	hash_map = {}
+	for c in clothes:
+		if c[1] in hash_map.keys():
+			hash_map[c[1]] += [c[0]]
+		else:
+			hash_map[c[1]] = [c[0]]
+
+	answer = 1
+	values = hash_map.values()
+	for val in values:
+		answer *= len(val) + 1
+	return answer - 1
+
+
+# print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
+# print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
+# print(solution(	[["a", "a"], ["b", "b"], ["c", "c"]]))
