6f203e66373efebcc26e016325412d4f36dce09c
Solji Choi <bssj9307@gmail.com>
2021-08-04 23:09:49 +0900

    210804 | LCS

---
+++ Baekjoon/백준 9251_LCS.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('9251.txt', 'r')
+
+s1, s2 = input(), input()
+lcs = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+for i in range(1, len(s1)+1):
+	for j in range(1, len(s2)+1):
+		if s1[i-1] == s2[j-1]:
+			lcs[i][j] = lcs[i - 1][j - 1] + 1
+		else:
+			lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
+
+print(max(sum(lcs, [])))
