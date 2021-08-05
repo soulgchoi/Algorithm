1a64dfeb8966e4be97b962ad86d9c82e586385ce
Solji Choi <bssj9307@gmail.com>
2021-08-05 12:48:06 +0900

    210805 | 단어 수학

--- Baekjoon/백준 1339_단어 수학.py
+++ Baekjoon/백준 1339_단어 수학.py
@@ -7,24 +7,54 @@ for tc in range(1, T+1):
 
 	N = int(input())
 	words = [input() for _ in range(N)]
-	w_max = max(len(x) for x in words)
-	for w in range(N):
-		words[w] = '0' * (w_max - len(words[w])) + words[w]
 
 	M = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 	alphabet = {}
 	for m in M:
-		alphabet[m] = '0'
+		alphabet[m] = 0
 
-	num = 9
-	for i in range(w_max):
-		for j in range(len(words)):
-			word = words[j]
-			if alphabet.get(word[i]) == '0':
-				alphabet[word[i]] = str(num)
-				num -= 1
-			word = word[0:i] + alphabet.get(word[i], '0') + word[i+1:]
-			words[j] = word
+	for word in words:
+		l = len(word)
+		for w in range(l):
+			alphabet[word[w]] += 10 ** (l - w - 1)
 
-	answer = sum(list(map(int, words)))
+	alphabet = sorted(alphabet.items(), key=lambda x: x[1], reverse=True)
+
+	answer = 0
+	number = 9
+	for a in alphabet:
+		x, y = a
+		if not y:
+			break
+
+		answer += number * y
+		number -= 1
 	print(answer)
+
+
+
+
+
+# N = int(input())
+# words = [input() for _ in range(N)]
+# w_max = max(len(x) for x in words)
+# for w in range(N):
+# 	words[w] = '0' * (w_max - len(words[w])) + words[w]
+#
+# M = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# alphabet = {}
+# for m in M:
+# 	alphabet[m] = '0'
+#
+# num = 9
+# for i in range(w_max):
+# 	for j in range(len(words)):
+# 		word = words[j]
+# 		if alphabet.get(word[i], '*') == '0':
+# 			alphabet[word[i]] = str(num)
+# 			num -= 1
+# 		word = word[0:i] + alphabet.get(word[i], '0') + word[i+1:]
+# 		words[j] = word
+#
+# answer = sum(list(map(int, words)))
+# print(answer)
